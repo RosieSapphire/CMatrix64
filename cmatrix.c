8db57ba709ab5187d1b10bbe9e84a9f6e296efb2
Rosie Sapphire <jayjaygames40@gmail.com>
2026-03-14 18:18:35 -0700

    Got text typing itself out with a fixed tickrate

--- cmatrix.c
+++ cmatrix.c
@@ -5,6 +5,10 @@
 
 #define FONT_JETBRAINS_MONO_BOLD 1
 
+#define TICKRATE 10u
+
+static const float tickrate_sec = 1.f / (float)TICKRATE;
+
 static const unsigned int text_pad_x = SCREEN_WIDTH / 4;
 static const unsigned int text_pad_y = SCREEN_WIDTH / 4;
 
@@ -30,8 +34,9 @@ static const rdpq_fontstyle_t fnt_style = {
 	.color = RGBA32(0xED, 0xAE, 0x49, 0xFF)
 };
 
-static bool	    start_pressed = false;
 static unsigned int text_progress = 0;
+static float	    text_timer	  = 0.f;
+static float	    time_accum	  = 0.f;
 
 int main(void)
 {
@@ -54,8 +59,11 @@ int main(void)
 	rdpq_text_register_font(FONT_JETBRAINS_MONO_BOLD, fnt);
 
 	/* Main loop */
-	for (; !start_pressed;) {
-		joypad_buttons_t btn_down;
+	for (;;) {
+		unsigned long	 ticks_old, ticks_new;
+		float		 time_diff;
+
+		ticks_old = get_ticks();
 
 		/* Render */
 		rdpq_attach_clear(display_get(), NULL);
@@ -63,11 +71,12 @@ int main(void)
 		rdpq_set_mode_fill(RGBA16(0x3, 0x6, 0x9, 0x1F));
 		rdpq_fill_rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-		rdpq_text_printf(&text_params,
+		rdpq_text_printn(&text_params,
 				 FONT_JETBRAINS_MONO_BOLD,
 				 text_pad_x / 2,
 				 text_pad_y / 2,
-				 text);
+				 text,
+				 text_progress);
 
 		rdpq_set_mode_standard();
 		rdpq_mode_blender(RDPQ_BLENDER_MULTIPLY);
@@ -75,10 +84,34 @@ int main(void)
 
 		rdpq_detach_show();
 
+		/* Calculate time */
+		ticks_new = get_ticks();
+		time_diff = (float)(ticks_new - ticks_old) /
+			    (float)TICKS_PER_SECOND;
+		time_accum += time_diff;
+
 		/* Update */
-		joypad_poll();
-		btn_down      = joypad_get_buttons_pressed(JOYPAD_PORT_1);
-		start_pressed = btn_down.start;
+		for (; time_accum >= tickrate_sec; time_accum -= tickrate_sec) {
+			joypad_buttons_t btn_down;
+
+			joypad_poll();
+			btn_down = joypad_get_buttons_pressed(JOYPAD_PORT_1);
+
+			/* Reset text progress with start button */
+			if (btn_down.start) {
+				text_progress = 0;
+				continue;
+			}
+
+			/* Advance text */
+			if (text_progress < text_len) {
+				++text_progress;
+				continue;
+			}
+
+			/* Cap text at maximum length */
+			text_progress = text_len;
+		}
 	}
 
 	/* Terminate */
